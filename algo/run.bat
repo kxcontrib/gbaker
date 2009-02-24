271977191f6b06483695eff0cba4bc18fa0fc1fe
gbaker <gbaker@b387b25f-291c-0410-8dc7-e51caacd5b47>
2009-02-24 11:40:42 +0000

    Added timer.q for single and recurring time updates

--- algo/run.bat
+++ algo/run.bat
@@ -7,5 +7,7 @@ start "market maker" /d \kdb \q\w32\q.exe qx/marketmaker.q  -p 2010
 ping 127.0.0.1 -n 2 -w 5000 > nul
 start "noise" /d \kdb \q\w32\q.exe qx/noise.q
 ping 127.0.0.1 -n 2 -w 5000 > nul
+start "timer" /d \kdb \q\w32\q.exe algo/timer.q -p 2012 -t 1000
+ping 127.0.0.1 -n 2 -w 5000 > nul
 start "participation" /d \kdb \q\w32\q.exe algo/trials.q -p 2011 -qx 2009
 #start "participation" /d \kdb \q\w32\q.exe algo/testparticipation.q -p 2011 -qx 2009
